6301f90b4cde87fd4c3f3a0637c1e8a1d46e9308
wacoom <30224833@qq.com>
2018-08-02 00:31:16 +0800

    我的页面

--- MiniProgram/src/pages/mine/index.vue
+++ MiniProgram/src/pages/mine/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    我的
-  </div>
+  <view class="user-card">
+    <view class="avatar-wrapper">
+      <image src="../../images/user-avatar.png"></image>
+
+    </view>
+    <view class="detail">
+      <view class="z-row">
+        <view class="name">kkkkkkkkkkk</view>
+        <view class="border">档次:123</view>
+      </view>
+      <view class="z-row">手机号：6502</view>
+    </view>
+  </view>
 </template>
 <script>
   export default {
@@ -15,4 +25,71 @@
     created () {}
   }
 </script>
-<style></style>
+<style>
+
+
+  .user-card {
+    display: flex;
+    background-color: #CF0C0C;
+    padding: 30rpx;
+  }
+  .detail {
+    color: white;
+    font-size: 28rpx;
+    margin-left: 60rpx;
+  }
+  .name {
+    font-size: 42rpx;
+    margin-right: 10rpx;
+  }
+  .z-row {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  .border {
+    border: 1px solid white;
+    padding-left: 8rpx;
+    padding-right: 8rpx;
+    height: 44rpx;
+    border-radius: 8rpx;
+  }
+  cell-flat {
+    width: 100%
+  }
+
+  .avatar-wrapper {
+    width: 120rpx;
+    height: 120rpx;
+    flex-shrink: 0;
+  }
+  .avatar-wrapper image{
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    border: 8rpx solid white;
+  }
+
+  .z-button {
+    height: 100rpx;
+    /* 立即付款: */
+    font-size: 36rpx;
+    color: #FFFFFF;
+    letter-spacing: 0.54rpx;
+    background: #CF0C0C;
+    border-radius: 12rpx;
+    width: 100%;
+    text-align: center;
+    line-height: 100rpx;
+  }
+  .blank .avatar-wrapper image{
+    border: 0
+  }
+
+  .blank .detail {
+    display: flex;
+    flex-grow: 1;
+    align-items: center;
+  }
+
+</style>
