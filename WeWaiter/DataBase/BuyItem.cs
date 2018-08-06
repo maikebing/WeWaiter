d09256450ce208e2df0736878b2799afb22f9956
MysticBoy <mysticboy@live.com>
2018-08-07 00:16:49 +0800

    第一次提交

---
+++ WeWaiter/DataBase/BuyItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WeWaiter.DataBase
+{
+    public class BuyItem
+    {
+        [Key]
+        public string BuyItemID { get; set; }
+        public string OrderID { get; set; }
+        public string GoodsID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Amount { get; set; }
+        public int Total { get; set; }
+       
+    }
+}
---
+++ WeWaiter/DataBase/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WeWaiter.DataBase
+{
+    public class User
+    {
+        [Key]
+        public string WeixinID { get; set; }
+        public string Phone { get; set; }
+        public string NickName { get; set; }
+        public DateTime JoinIn { get; set; }
+        public DateTime LastActive { get; set; }
+
+    }
+}
