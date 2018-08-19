cedbff47eb4d5833808ad00ba933175ef4d1598d
wangqian <1510106069@qq.com>
2018-08-20 01:03:59 +0800

    用餐人数及用餐方式组件

--- MiniProgram/src/components/diningdetails/diningdetails.vue
+++ MiniProgram/src/components/diningdetails/diningdetails.vue
@@ -1,29 +1,71 @@
 <template>
-  <div class="dining-details">
-    <div class="weui-cell weui-cell_select">
-      <div class="weui-cell__hd weui-cell__hd_in-select-after">
-        <div class="weui-label">用餐人数</div>
+  <div class="dining-details-box">
+    <div class="dining-details">
+      <div class="weui-cell weui-cell_select">
+        <div class="weui-cell__hd weui-cell__hd_in-select-after">
+          <div class="weui-label">用餐人数</div>
+        </div>
+        <div class="weui-cell__bd">
+          <picker @change="bindPickerChange" :value="index" :range="diningDetailsList" range-key="name">
+            <div class="weui-select weui-select_in-select-after">{{diningDetailsList[index].name}}</div>
+          </picker>
+        </div>
       </div>
-      <div class="weui-cell__bd">
-        <picker @change="bindPickerChange" :value="index" :range="array">
-          <div class="weui-select weui-select_in-select-after">{{array[index]}}</div>
-        </picker>
-      </div>
+    </div>
+    <div class="weui-cells weui-cells_after-title">
+      <radio-group @change="bindRadioChange">
+        <label class="weui-cell weui-check__label" v-for="item in radioItems" :key="value">
+          <radio class="weui-check" :value="item.value" :checked="item.checked"/>
+
+          <div class="weui-cell__hd weui-check__hd_in-checkbox">
+            <icon class="weui-icon-checkbox_circle" type="circle" size="23" v-if="!item.checked"></icon>
+            <icon class="weui-icon-checkbox_success" type="success" size="23" v-if="item.checked"></icon>
+          </div>
+          <div class="weui-cell__bd">{{item.name}}</div>
+        </label>
+      </radio-group>
     </div>
   </div>
 </template>
 
 <script>
   export default {
+    props: {
+      diningDetailsList: {
+        type: Array,
+        default: function () {
+          return [
+            {
+              id: '',
+              name: ''
+            }
+          ]
+        }
+      }
+    },
     data () {
       return {
         index: 0,
-        array: ['美国', '中国', '巴西', '日本']
+        radioItems: [
+          {name: '店内用餐', value: '0'},
+          {name: '打包带走', value: '1', checked: true}
+        ]
       }
     },
     methods: {
-      bindPickerChange: function(e) {
-        console.log('picker发送选择改变，携带值为', e.detail.value)
+      bindPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.target.value)
+        let index = e.target.value
+        this.$emit('select-tableware', this.diningDetailsList[index].id)
+      },
+      bindRadioChange: function (e) {
+        console.log('radio发生change事件，携带value值为：', e)
+        let radioItems = this.radioItems
+        for (let i = 0, len = radioItems.length; i < len; ++i) {
+          radioItems[i].checked = radioItems[i].value === e.target.value
+        }
+        this.radioItems = radioItems
+        this.$emit('select-meal-style', e.target.value)
       }
     },
     created () {
@@ -33,9 +75,14 @@
 
 <style lang="scss" scoped>
   @import "~@/styles/variable";
-  .dining-details {
+
+  .dining-details-box {
     background-color: $primary-white;
-    padding: 0 30px;
+    padding: 0 30px 0;
+    .dining-details {
+      padding: 30px 0;
+      border-bottom: 1px solid #D3D3D3;
+    }
   }
 
   .weui-select {
@@ -46,11 +93,32 @@
     font-size: 36px;
     color: #1F1F21;
   }
+
   .weui-label {
     /* 用餐人数: */
     width: auto;
     font-size: 32px;
     color: #1F1F21;
     letter-spacing: -0.27px;
+  }
+
+  .weui-cells {
+    position: static;
+    border-bottom-width: 0;
+  }
+  radio-group {
+    display: flex;
+    padding: 38px 0 44px;
+    /* 店内用餐: */
+    font-size: 32px;
+    color: #1F1F21;
+    letter-spacing: -0.27px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border-bottom-width: 0;
+    .weui-cell {
+      position: static;
+    }
   }
 </style>
--- MiniProgram/src/pages/checkout/index.vue
+++ MiniProgram/src/pages/checkout/index.vue
@@ -6,7 +6,10 @@
       <menu-list :menu-list="selectFoods" :total="total"></menu-list>
     </div>
     <div class="we-dining-details-box">
-      <dining-details></dining-details>
+    <dining-details
+      :dining-details-list="diningDetailsList"
+      @select-meal-style="onSelectMealStyle"
+      @select-tableware="onSelectTableWare"></dining-details>
     </div>
     <div class="we-button-fixed-bottom" @click="toPay()">
       微信支付 ￥{{total}}
@@ -16,21 +19,32 @@
 <script type="text/babel">
   import OrderStatus from '@/components/OrderListItemStatus'
   import TableNumber from '@/components/MealTableNumber'
-  import DiningDetails from '@/components/diningdetails/diningdetails'
   import {orderList} from '@/mock/mockOrderData'
+  import DiningDetails from '@/components/diningdetails/diningdetails'
   import MenuList from '@/components/MealMenuList'
   export default {
     data () {
       return {
         order: {},
         selectFoods: [],
+        // 餐具数量id
+        tableWareId: '',
+        // 用餐方式
+        mealStyleId: '',
         seller: {
           avatar: ''
-        }
+        },
+        index: 0,
+        diningDetailsList: [
+          {
+            id: '',
+            name: ''
+          }
+        ]
       }
     },
-    computed:{
-      total: function() {
+    computed: {
+      total: function () {
         return this.selectFoods.reduce((acc, current) => acc += current.price * current.count, 0)
       }
     },
@@ -41,6 +55,17 @@
       DiningDetails
     },
     methods: {
+      bindPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail)
+      },
+      onSelectMealStyle (value) {
+        console.warn('发生用餐方式选择事件', value)
+        this.mealStyleId = value
+      },
+      onSelectTableWare (value) {
+        console.warn('发生餐具选择事件', value)
+        this.tableWareId = value
+      },
       async getData (id) {
         console.log('getData@37', id)
         let orderRes = await this.$http.get(`order/${id}`)
@@ -48,14 +73,20 @@
         this.order = orderRes.data
         this.selectFoods = orderRes.data.foods
       },
+      async getDiningDetailsListData () {
+        let diningDetailsListRes = await this.$http.get('diningdeails')
+        this.diningDetailsList = diningDetailsListRes.data
+      },
       async toPay () {
         wx.showLoading({
-          title: '订单创建中',
+          title: '订单创建中'
         })
         let orderParams = {
           codeid: 'ww12354325431',
           openid: 'wx12314351432',
-          foods: this.selectFoods
+          foods: this.selectFoods,
+          tableWareId: this.tableWareId,
+          mealStyleId: this.mealStyleId
         }
         let orderRes = await this.$http.post('order', orderParams)
         console.log('toPay@44', orderRes)
@@ -81,6 +112,7 @@
         this.selectFoods = wx.getStorageSync('selectFoods')
         this.seller = wx.getStorageSync('seller')
       }
+      this.getDiningDetailsListData()
     }
   }
 </script>
