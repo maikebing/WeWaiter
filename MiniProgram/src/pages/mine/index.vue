b9c3cc7b04737eaf82bb5b8db495e4c80b0a98db
王倩 <1510106069@qq.com>
2018-09-11 17:04:11 +0800

    展示微信用户信息

--- MiniProgram/src/pages/mine/index.vue
+++ MiniProgram/src/pages/mine/index.vue
@@ -1,25 +1,39 @@
 <template>
   <view class="user-card">
     <view class="avatar-wrapper">
-      <image src="/static/images/user-avatar.png"></image>
+      <image :src="user.avatarUrl"></image>
     </view>
     <view class="detail">
       <view class="z-row">
-        <view class="name">漩涡鸣人</view>
+        <view class="name">{{user.nickName}}</view>
         <!--<view class="border">档次:123</view>-->
       </view>
-      <view class="z-row">手机号：13999564234</view>
+      <!--<view class="z-row">手机号：13999564234</view>-->
     </view>
   </view>
 </template>
 <script>
   export default {
     data () {
-      return {}
+      return {
+        user: {
+          nickName: '',
+          avatarUrl: ''
+        }
+      }
     },
     components: {
     },
     methods: {
+      getUserData () {
+        let user = wx.getStorageSync('user')
+        if (user) {
+          this.user = user.wechat
+        }
+      }
+    },
+    onShow () {
+      this.getUserData()
     },
     created () {}
   }
@@ -28,19 +42,19 @@
   @import "~@/styles/variable";
   .user-card {
     display: flex;
+    flex-direction: column;
+    align-items: center;
     /* Mask: */
     background-image: linear-gradient(-90deg, #FE9270 1%, #F26032 100%);
     padding: 60px;
   }
   .detail {
+    margin-top: 20px;
     color: white;
     font-size: 28px;
-    margin-left: 60px;
   }
   .name {
-    font-size: 42px;
-    margin-right: 10px;
-    font-weight: bold;
+    font-size: 34px;
   }
   .z-row {
     display: flex;
@@ -60,15 +74,16 @@
   }
 
   .avatar-wrapper {
-    width: 120px;
-    height: 120px;
+    width: 145px;
+    height: 145px;
+    border-radius: 100%;
+    border: 8px solid white;
+    overflow: hidden;
     flex-shrink: 0;
   }
   .avatar-wrapper image{
     width: 100%;
     height: 100%;
-    border-radius: 100%;
-    border: 8px solid white;
   }
 
   .z-button {
